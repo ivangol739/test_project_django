5fdb2cbb5a862d61c2ff4a6e445ff34a4bf54b79
ivangol739 <ivangol739@gmail.com>
2025-01-16 22:44:49 +0300

    add views orm mrthod

--- orm_method/views.py
+++ orm_method/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from orm_method.models import Worker
 
-# Create your views here.
+
+def index(request):
+    people = Worker.objects.all()
+    return render(request, 'orm_method/index.html', {'people': people})
+
+
